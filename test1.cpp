e7b90bba14f3133a659694d66b304c982211b61e
KarinaBon <78025106+KarinaBon@users.noreply.github.com>
2021-03-19 16:56:33 +0200

    Add files via upload

---
+++ test1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <math.h>
+#include <cstdlib>
+
+/*int main () {
+Виконання завдання №1. Варіант №2, студент групи ФАЕТ-109, Бондарчук Карина Сергіївна
+
+	L = pow((1 / tan(c)),2) + (2 * pow(x, 2) + 5)/(sqrt(c + t));
+	std::cout << L;
+	*/
+
+/*int main () {
+Виконання завдання №2. Варіант №2, студент групи ФАЕТ-109, Бондарчук Карина Сергіївна
+	int N;
+	std::cout << "N = ";
+	std::cin >> N;
+	
+	if (N % 4 == 0 || N % 7 == 0) {
+		std::cout << "true";	
+	}
+	else {
+		std::cout << "false";	
+	}			
+}
+	*/
+/*
+int main () {
+Виконання завдання №3. Варіант №2, студент групи ФАЕТ-109, Бондарчук Карина Сергіївна
+	for(int i = 2; i <= 100; i++) {
+		if (i % 2 == 0 && i % 5 == 0)
+		std::cout << i << " ";
+	}
+				
+}
+*/
+
+/*
+int main () {
+// Виконання завдання №4. Варіант №2, студент групи ФАЕТ-109, Бондарчук Карина Сергіївна
+	const int n = 10;
+	int i, arr[n], average;
+	int sum = 0;
+	
+	for(i = 0; i < n; i++) {
+	arr[n] = 1 + rand() % 50;
+	std::cout << arr[n] << " ";
+	}
+	std::cout << std::endl;
+	for(i = 0; i <= n; i++) {
+		sum += arr[n];
+		average = sum / n;
+	}
+
+}
+*/
